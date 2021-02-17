66f590cb3a3f982e6c09b95a114c6919829b1f68
Brock <bruceanthonyii@gmail.com>
2021-02-17 00:16:29 -0600

    coming back to this later

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,132 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const instruction = "The NPM package required to run Readme Generator application is Inquirer";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the name of the app?", "What is the purpose of the app?", "What NPM packages and technologies does the app use?", "How do you use the app?", "Please provide contribution guidelines.", "Please provide test instructions."];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile('./dist/README.md', fileName, err => {
+        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+          reject(err);
+          // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+          return;
+        }
+  
+        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+        resolve({
+          ok: true,
+          message: 'README.md created!'
+        });
+      });
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log(instruction);
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'appName',
+          message: questions[0],
+          validate: appNameInput => {
+            if (appNameInput) {
+              return true;
+            } else {
+              console.log('Please enter your applcation name!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: questions[1],
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('Please provide a description of the application!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'technology',
+          message: questions[2],
+          validate: technologyInput => {
+            if (technologyInput) {
+              return true;
+            } else {
+              console.log('Please provide the technologies used in the application!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: questions[3],
+          validate: usageInput => {
+            if (usageInput) {
+              return true;
+            } else {
+              console.log('Please provide instructions on how to use the application!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'guidelines',
+          message: questions[4],
+          validate: guidelinesInput => {
+            if (guidelinesInput) {
+              return true;
+            } else {
+              console.log('Please provide contribution guidelines for the application!');
+              return false;
+            }
+          }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[5],
+            validate: testInput => {
+                if (testInput) {
+                return true;
+                } else {
+                console.log('Please provide intructions for testing the functionality of the application!');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'licence',
+            message: 'What licence does this app have? (Check one)',
+            choices: ["PD", "CC0", "MIT", "Apache", "MPL", "GPL", "AGPL", "JRL", "AFPL"]
+        }
+
+        // {
+        //   type: 'confirm',
+        //   name: 'confirmLicence',
+        //   message: 'Would you like to enter enter licencing information for the app?',
+        //   default: true
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'about',
+        //   message: 'Provide some information about yourself:',
+        //   when: ({ confirmAbout }) => confirmAbout
+        // }
+      ]);
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,55 @@
+
+//const license = ["PD", "CC0", "MIT", "Apache", "MPL", "GPL", "AGPL", "JRL", "AFPL"]
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "PDDL":
+      return "[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)";
+    case "CC0":
+      return "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "Apache":
+      return "[![License Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "MPL":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    case "GPL":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "AGPL":
+      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+    case "no license":
+      return "";
+  }
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "PDDL":
+      return "[License: PDDL](https://opendatacommons.org/licenses/pddl/)";
+    case "CC0":
+      return "[License: CC0](http://creativecommons.org/publicdomain/zero/1.0/)";
+    case "MIT":
+      return "[License: MIT](https://opensource.org/licenses/MIT)";
+    case "Apache":
+      return "[License Apache](https://opensource.org/licenses/Apache-2.0)";
+    case "MPL":
+      return "[License: MPL](https://opensource.org/licenses/MPL-2.0)";
+    case "GPL":
+      return "[License: GPL](https://www.gnu.org/licenses/gpl-3.0)";
+    case "AGPL":
+      return "[License: AGPL](https://www.gnu.org/licenses/agpl-3.0)";
+    case "no license":
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -18,3 +59,5 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+console.log(renderLicenseBadge)
